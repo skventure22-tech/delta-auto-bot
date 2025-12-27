51b01e8b5db4d6ab22c9b375acd2a9252107425a
skventure22-tech <skventure22@gmail.com>
2025-12-27 20:22:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.get("/health", (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
